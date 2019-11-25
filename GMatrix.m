64e48ea16bc7dd9e783cfc15349d69304e1641ce
tatalessap <martapibirimp@gmail.com>
2019-11-25 15:01:51 +0100

    HH + Givens

---
+++ GMatrix.m
@@ -0,0 +1,8 @@
+function [G] = GMatrix(c, s, i, j, n)
+    G = speye(n);
+    G(i, i) = c;
+    G(i, j) = s;
+    G(j, j) = -s;
+    G(j, i) = c;
+end
+
---
+++ GivRot.m
@@ -0,0 +1,18 @@
+function [c, s] = GivRot(xi, xj)
+    if xj == 0 
+        c = 1;
+        s = 0;
+    else
+        if abs(xi)>abs(xj)
+            t = xi/xj; 
+            z = sqrt(1+t^2);
+            s = 1/z;
+            c = t*s;
+        else
+            t = xj/xi;
+            z = sqrt(1+t^2);
+            c = 1/z;
+            s = t * c;
+        end
+    end
+    
