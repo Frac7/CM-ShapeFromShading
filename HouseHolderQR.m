094033fdd56f7bf75c309ab581f9e70a56fb83bc
Frac7 <francescacella96@gmail.com>
2019-11-25 12:31:40 +0100

    HouseHolderMatrix in a separated file

--- HouseHolderQR.m
+++ HouseHolderQR.m
@@ -6,7 +6,7 @@ Q = eye(n);
 
 for i = 1 : n %n al posto di n-1 per matrice rettangolare
     X = A (i: n, i);
-    [w, k, Ht] = HouseMatrix(X); %Ht per dire htilde per dire che è grande quanto x
+    [w, k, Ht] = HouseHolderMatrix(X); %Ht per dire htilde per dire che è grande quanto x
     H = [eye(i-1) zeros(i-1, n-i+1); zeros(n-i+1, i-1) Ht];
     
     A = H * A; %questo dovrebbe aggiungere una colonna di zeri
@@ -18,29 +18,4 @@ for i = 1 : n %n al posto di n-1 per matrice rettangolare
     Q = Q * H; %questo aggiunge H a destra della Q
 end
 
-end
-
-function [w, k, H] = HouseMatrix(A)
-n = size(A, 1);
-sigma = norm(A);
-
-if A(1) >= 0
-    k = -sigma;
-else
-    k = sigma;
-end
-
-lambda = sqrt (2 * sigma * (sigma + abs(A(1))));
-
-%conviene togliere k dalla prima componente e non se ne parla più
-
-w = A;
-
-w(1) = w(1) - k;
-
-w = w / lambda;
-
-if nargout > 2  
-    H = eye(n) - 2 * (w * w');
-end
 end
