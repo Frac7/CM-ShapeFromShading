7038b03d60f81142c3e1ada8b376012984b2ba90
tatalessap <martapibirimp@gmail.com>
2019-12-09 12:56:10 +0100

    fix e end first part

--- TestPseudoInverse.m
+++
@@ -1,6 +0,0 @@
-%% TestPseudoInverse
-% Calcolo dell'errore della pseudoinversa
-%%
-function [e] = TestPseudoInverse(A)
-    e = norm(pinv(A) - PseudoInverse(A));
-end
--- test2PartWithQR.m
+++ test2PartWithQR.m
@@ -1,4 +1,11 @@
+%% SECONDA PARTE
+% Grazie ai dati prestati dal docente, verifichiamo prima con risoluzione di
+% un sistema con fattorizzazione QR, se riusciamo ad ottenere una N
+% (superfici normali) di buona qualità.
+%%
+addpath Functions/
 load superficie1.mat
+format short e
 
 %ora ho L M N r s
 
@@ -7,19 +14,68 @@ Ltrasp = L';
 
 Mtrasp = M';
 
+%% con HouseHolder
 %fatt QR di Ltrasp risoluzione sist minimi quadrati QR
-[Q, R] = HouseHolderQR(Ltrasp);
+[Qh, Rh] = HouseHolderQR(Ltrasp);
 
-C = Q' * Mtrasp;
+Nqrtesth = QRSystemResolution(Qh, Rh, Mtrasp);
 
-Nqrtest= R\C;
+%% con Givens
+%fatt QR di Ltrasp risoluzione sist minimi quadrati QR
+[Qg, Rg] = GivensQR(Ltrasp);
 
-%matlab
+% Cg = Qg' * Mtrasp;
+% 
+% Nqrtestg= Rg\Cg;
+
+Nqrtestg = QRSystemResolution(Qg, Rg, Mtrasp);
+
+%% matlab
 Nmatlab = Ltrasp\Mtrasp;
 
-%err confronto Nqrtest vs N 
-errorNqrN= norm(N - Nqrtest);
-%err confronto Nmatlab vs N-prof
-errorNmatN= norm(N - Nmatlab);
-%err confronto Nqrtest vs Nmatlab
-errorNqrNmat= norm(Nmatlab - Nqrtest);
+%% errore nella fattorizzazione
+errFatt = norm(N'*L-M, inf);
+errFattH = norm(Nqrtesth'*L-M, inf);
+errFattG = norm(Nqrtestg'*L-M, inf);
+errFattMat = norm(Nmatlab'*L-M, inf);
+
+disp('')
+disp('errore nella fattorizzazione')
+disp([errFatt errFattH errFattG errFattMat])
+
+%% errore sulle noramli
+errNh = norm(N - Nqrtesth, inf);
+errNg = norm(N - Nqrtestg, inf);  
+errNmat = norm(N - Nmatlab, inf);
+% come se stessimo prendendo per buona quella di matlab
+errNmath = norm(Nmatlab - errNh, inf);
+errNmatg = norm(Nmatlab - errNg, inf);
+
+disp('')
+disp('errore sulle normali')
+disp([errNh errNg errNmat errNmath errNmatg])
+
+%% errore relativo sulle normali
+errNhRel = norm(N - Nqrtesth, inf)/ norm(N, inf);
+errNgRel = norm(N - Nqrtestg, inf)/ norm(N, inf) ;  
+errNmatRel = norm(N - Nmatlab, inf)/ norm(N, inf);
+% come se stessimo prendendo per buona quella di matlab
+errNmathRel = norm(Nmatlab - errNh, inf) / norm(Nmatlab, inf);
+errNmatgRel = norm(Nmatlab - errNg, inf) / norm(Nmatlab, inf);
+
+disp('')
+disp('errore relativo sulle normali')
+disp([errNhRel errNgRel errNmatRel errNmathRel errNmatgRel])
+
+%stampa
+figure
+title('Relative Error, Normals');
+xlabel('Resolution Methods');
+ylabel('Relative Error');
+    
+bar(categorical({'Householder', 'Givens', 'Matlab'}),[errNhRel errNgRel errNmatRel]);
+legend('error');
+
+
+
+
