7e42c6e1ff37d46bc06191ffad7b9b52f3f56499
tatalessap <martapibirimp@gmail.com>
2019-12-09 17:51:36 +0100

    add times in the new tests

--- test2PartWithSVD.m
+++ test2PartWithSVD.m
@@ -8,11 +8,18 @@ format short e
 
 %ora ho L M N r s
 
+tic
+
 Lps=pinv(L);
+
+%N'*L = M
+%N' = M * Lps
 
 Ntrasp = M * Lps;
 
 Nsvdtest = Ntrasp';
+
+timeSvd = toc;
 
 %% errore nella fattorizzazione
 errFatt = norm(N'*L-M, inf);
@@ -26,14 +33,17 @@ errNsvd = norm(N - Nsvdtest, inf);
 
 disp('')
 disp('errore sulle normali')
-disp([errNsvd])
+disp(errNsvd)
 
 %% errore relativo sulle normali
 errNRelsvd = norm(N - Nsvdtest, inf)/ norm(N, inf);
 
 disp('')
 disp('errore relativo sulle normali')
-disp([errNRelsvd])
+disp(errNRelsvd)
+
+disp('')
+disp(timeSvd)
 
 
 %%
